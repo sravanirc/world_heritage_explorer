612e0d40ce7da0b00051e585d3ea08552303cc9e
Sravani Ravuri <108229743+sravanirc@users.noreply.github.com>
2025-01-27 09:55:52 +0000

    Delete config/.env.exs
    Deleting .env

--- config/.env.exs
+++
@@ -1,6 +0,0 @@
-
-System.put_env("DB_HOST", "localhost")
-System.put_env("DB_USER", "postgres")
-System.put_env("DB_PASS", "postgres")
-System.put_env("DB_NAME", "world_heritage_explorer_dev")
-System.put_env("DB_PORT", "5432")
